4765fc889bdd65f67b1d65daee99ac0b2b75b0c6
Sudhir Sapkal <sudhirjsapkal@gmail.com>
2019-01-15 11:32:19 +0530

    Indexing and Slicing

--- 02Strings/indexing_slicing.py
+++ 02Strings/indexing_slicing.py
@@ -3,3 +3,11 @@
 #Substring. Get the characters from position 2 to position 5 (not included)
 
 
+my_name = "Sudhir Sapkal"
+
+#Indexing can be done on String
+print(my_name[1])
+
+#Slicing can be done on string
+print(my_name[2:5])
+
