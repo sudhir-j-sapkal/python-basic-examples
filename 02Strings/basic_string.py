547fa58e7d4407e3034bc8b17ad3f9f46aaee444
Sudhir Sapkal <sudhirjsapkal@gmail.com>
2019-01-15 11:30:53 +0530

    Only String Declaration and Usage

--- 02Strings/basic_string.py
+++ 02Strings/basic_string.py
@@ -2,10 +2,3 @@ first_name = "Sudhir"
 last_name = "Sapkal"
 
 print("My Name Is "+first_name+" "+last_name)
-
-#Indexing can be done on String
-print(first_name[4])
-
-#Slicing can be done on string
-print(last_name[1:6])
-
